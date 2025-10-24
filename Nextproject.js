9c352c2c05c4be05904d15873ad0b4e8254a1605
seelananton8-byte <seelananton8@gmail.com>
2025-10-24 18:35:40 +0530

    Added a project

--- Nextproject.js
+++ Nextproject.js
@@ -1,1 +1,12 @@
-
+function car (brand, model, color){
+    this.brand = brand;
+    this.model = model;
+    this.color = color;
+    this.start = function(){
+        console.log(`${this.brand} ${this.model} is starting.....`);
+    };
+}
+const car1 = newCar("Toyota", "Corolla", "Silver");
+const car2 = newCar("Honda", "Civic", "Black");
+car1.start();
+car2.start();
